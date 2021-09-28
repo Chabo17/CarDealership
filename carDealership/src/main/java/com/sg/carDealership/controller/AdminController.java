c929451d0626566d985cd53024fc77c0cdc98c4e
Pierre L'Espérance <polpolzor@hotmail.com>
2021-09-28 16:52:04 -0400

    IMPLEMENTED ADD NEW CAR LINK TO DB FULLY FUNCTIONAL // POL

--- carDealership/src/main/java/com/sg/carDealership/controller/AdminController.java
+++ carDealership/src/main/java/com/sg/carDealership/controller/AdminController.java
@@ -74,6 +74,67 @@ public class AdminController {
         return "cars";
     }
     
+    @PostMapping("addCar")
+    public String addCarRequest(String make, String model, String carType, String bodyStyle, String year, 
+                                String trans, String color, String interiorColor, String mileage, String vinNumber,
+                                String msrp, String salesPrice, String description, String carName, String pictureURL)
+    {
+        Cars newcar = new Cars();
+        
+                                                                        newcar.setMake(make);
+                                                                        newcar.setModel(model);
+                                                                        newcar.setCarType(carType);
+                                                                        newcar.setBodyStyle(bodyStyle);
+                                                                        newcar.setMakeYear(Integer.parseInt(year));
+                                                                        newcar.setTrans(trans);
+                                                                        newcar.setColor(color);
+                                                                        newcar.setInteriorColor(interiorColor);
+                                                                        newcar.setMileage(Integer.parseInt(mileage));
+                                                                        newcar.setVinNumber(vinNumber);
+                                                                        newcar.setMsrp(Double.parseDouble(msrp));
+                                                                        newcar.setSalesPrice(Double.parseDouble(salesPrice));
+                                                                        newcar.setCarDescription(description);
+                                                                        newcar.setNewCar(newcar.getMakeYear() == 2021);
+        
+        
+        newcar.setCarName(carName);
+        newcar.setPictureURL(pictureURL);
+        
+        
+        carsDao.addCar(newcar);
+        
+        return "redirect:/featured";
+    }
+    
+    @GetMapping("addCar")
+    public String addCarForm(Model model) {
+        String[] makeList = {"Audi", "Honda", "China"};//dao get list of items
+        model.addAttribute("makeList", makeList);
+        
+        String[] modelList = {"A4", "S", "S3"};
+        model.addAttribute("modelList", modelList);
+        
+        String[] typeList = {"New", "Used", "Broken"};
+        model.addAttribute("typeList", typeList);
+        
+        String[] bodyList = {"Car", "SUV", "COUPE"};
+        model.addAttribute("bodyList", bodyList);
+        
+        String[] transList = {"Automatic", "Manual"};
+        model.addAttribute("transList", transList);
+        
+        String[] colorList = {"Black", "Red", "White"};
+        model.addAttribute("colorList", colorList);
+        
+        String[] interList = {"Gold", "Black", "Silver"};
+        model.addAttribute("interList", interList);
+        
+        
+        //List<Cars> cars = carsDao.getAllCars();
+        //model.addAttribute("cars", cars);
+        return "addCar";
+    }
+    
     @GetMapping("users")
     public String displayUsers(Model model) {
         //List<Cars> cars = carsDao.getAllCars();
@@ -81,6 +142,8 @@ public class AdminController {
         return "users";
     }
     
+    /*
+
     @PostMapping("addCars")
     public String addCar(Cars car, HttpServletRequest request) {
         String makeChoice = request.getParameter("makelist");
@@ -98,6 +161,9 @@ public class AdminController {
 //        
         return "redirect:/admin/cars";
     }
+
+    
+    */
     
     
     
