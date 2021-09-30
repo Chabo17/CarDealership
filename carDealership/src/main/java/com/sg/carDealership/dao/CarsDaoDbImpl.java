552ae69671525ef6add7cd091fcd588d9c2e9de9
jantotong <tongjantoo@gmail.com>
2021-09-30 14:03:06 -0700

    Bug in Cars Dao

--- carDealership/src/main/java/com/sg/carDealership/dao/CarsDaoDbImpl.java
+++ carDealership/src/main/java/com/sg/carDealership/dao/CarsDaoDbImpl.java
@@ -42,13 +42,13 @@ public class CarsDaoDbImpl implements CarsDao {
     @Override
     public List<Cars> getAllNewCars() 
     {
-        final String SELECT_ALL_NEW_CARS = "SELECT * FROM cars WHERE newCar = 1";
+        final String SELECT_ALL_NEW_CARS = "SELECT * FROM cars WHERE carType = 'New' ";
         return jdbc.query(SELECT_ALL_NEW_CARS, new CarMapper());
     }
 
     @Override
     public List<Cars> getAllUsedCars() {
-        final String SELECT_ALL_NEW_CARS = "SELECT * FROM cars WHERE newCar = 0";
+        final String SELECT_ALL_NEW_CARS = "SELECT * FROM cars WHERE carType = 'Used'";
         return jdbc.query(SELECT_ALL_NEW_CARS, new CarMapper());
     }
 
