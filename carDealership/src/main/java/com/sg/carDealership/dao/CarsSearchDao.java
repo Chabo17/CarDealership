474c372090476416808c5ed2c902816d56e2f4c6
Pierre L'Espérance <polpolzor@hotmail.com>
2021-09-30 11:40:44 -0400

    fully implemented CarsSearch for admin view // POL

---
+++ carDealership/src/main/java/com/sg/carDealership/dao/CarsSearchDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.carDealership.dao;
+
+import com.sg.carDealership.dto.Cars;
+import java.util.List;
+
+/**
+ *
+ * @author pierre
+ */
+public interface CarsSearchDao 
+{
+    
+    List<Cars> getfilteredCars(String keyword);
+    
+    
+    
+}
